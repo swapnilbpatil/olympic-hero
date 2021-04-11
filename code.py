fb1f8f36d7bb1504aeabbb8e0fcf1e6ba379e52f
Swapnil patil <81431059+swapnilbpatil@users.noreply.github.com>
2021-04-11 11:57:39 +0530

    Code file created.

---
+++ code.py
@@ -0,0 +1,111 @@
+# --------------
+#Importing header files
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+#Path of the file
+path
+
+#Code starts here
+data = pd.read_csv(path)
+data.rename(columns = {"Total": "Total_Medals"}, inplace = True) 
+data.head(10)
+
+
+# --------------
+#Code starts here
+
+
+
+
+
+data['Better_Event']=np.where(data['Total_Summer']>data['Total_Winter'],'Summer',np.where(data['Total_Summer']==data['Total_Winter'],'Both','Winter'))
+better_event=data['Better_Event'].value_counts().idxmax()
+print(better_event)
+
+
+
+# --------------
+#Code starts here
+
+
+
+
+
+top_countries = data [['Country_Name','Total_Summer', 'Total_Winter','Total_Medals']]
+top_countries=top_countries[:-1]
+def top_ten(data,col):
+    country_list=[]
+    country_list=list ((data.nlargest(10,col)['Country_Name']))
+    return country_list
+top_10_summer = top_ten(top_countries,'Total_Summer')   
+top_10_winter = top_ten(top_countries,'Total_Winter')
+top_10 = top_ten(top_countries,'Total_Medals')
+common = list(set(top_10_summer) & set(top_10_winter) & set(top_10))
+
+
+
+# --------------
+#Code starts here
+
+
+
+
+summer_df=data[data['Country_Name'].isin(top_10_summer)]
+winter_df=data[data['Country_Name'].isin(top_10_winter)]
+top_df=data[data['Country_Name'].isin(top_10)]
+plt.figure(figsize=(15, 5))
+plt.bar(summer_df['Country_Name'], summer_df['Total_Summer'])
+plt.title('Top 10 Summer')
+plt.xlabel('Country Name')
+plt.ylabel('Total Medals')
+
+
+
+# --------------
+#Code starts here
+summer_df['Golden_Ratio']=summer_df['Gold_Summer']/summer_df['Total_Summer']
+summer_max_ratio=max(summer_df['Golden_Ratio'])
+summer_country_gold=summer_df.loc[summer_df['Golden_Ratio'].idxmax(),'Country_Name']
+
+winter_df['Golden_Ratio']=winter_df['Gold_Winter']/winter_df['Total_Winter']
+winter_max_ratio=max(winter_df['Golden_Ratio'])
+winter_country_gold=winter_df.loc[winter_df['Golden_Ratio'].idxmax(),'Country_Name']
+
+top_df['Golden_Ratio']=top_df['Gold_Total']/top_df['Total_Medals']
+top_max_ratio=max(top_df['Golden_Ratio'])
+top_country_gold=top_df.loc[top_df['Golden_Ratio'].idxmax(),'Country_Name']
+
+
+# --------------
+#Code starts here
+data_1=data[:-1]
+
+
+
+
+data_1['Total_Points']=data_1['Gold_Total']*3+data_1['Silver_Total']*2+data_1['Bronze_Total']
+most_points=max(data_1['Total_Points'])
+best_country=data_1.loc[data_1['Total_Points'].idxmax(),'Country_Name']
+
+
+# --------------
+#Code starts here
+best=data[data['Country_Name']==best_country]
+best.reset_index(drop = True, inplace = True)
+best=best[['Gold_Total','Silver_Total','Bronze_Total']]
+best.plot.bar(stacked=True)
+plt.xlabel('United States')
+plt.ylabel('Medals Tally')
+plt.xticks(rotation=45)
+l=plt.legend()
+l.get_texts()[0].set_text('Gold_Total :' + str(best['Gold_Total'].values))
+l.get_texts()[1].set_text('Silver_Total :' + str(best['Silver_Total'].values))
+l.get_texts()[2].set_text('Bronze_Total :' + str(best['Bronze_Total'].values))
+
+
+
+
+
+
